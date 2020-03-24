74e624a031a40163790d589cf9bec0ec23c9e29a
G2440 <gauravnegi2440@gmail.com>
2020-03-24 17:38:19 +0530

    24 march 2

---
+++ hexa.cpp
@@ -0,0 +1,12 @@
+//Ladder11--:>Hexadecimal's Theorem 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    long int x;
+    cin>>x;
+    cout<<0<<" " <<0<< " "<<x<<endl;
+    return 0;
+}
