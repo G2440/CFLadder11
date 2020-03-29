e1044276c0b568e0b901d1759ebd366dfc9bc5db
G2440 <gauravnegi2440@gmail.com>
2020-03-29 22:05:54 +0530

    29 March  2

--- dragon.cpp
+++ dragon.cpp
@@ -10,6 +10,20 @@ int main(){
     for(int i =0 ; i < y ; i++)
     cin>>str[i]>>bon[i];
     int flag =0 ;
+   
+    for(int i =0 ; i < y-1; i++){
+        for(int j = i +1 ; j < y;j++){
+            if(str[i]>str[j]){
+                int temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+                int temp1 = bon[i];
+                bon[i] = bon[j];
+                bon[j] = temp1;
+            }
+        }
+    }
+
     for(int i = 0 ; i < y; i++){
         if(x>str[i])
         x+= bon[i];
