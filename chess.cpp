056e8b1c926db3bf1b55bb8c8e0d034fa740e65a
G2440 <gauravnegi2440@gmail.com>
2020-03-31 22:36:07 +0530

    31 march 1

---
+++ chess.cpp
@@ -0,0 +1,39 @@
+//Ladder11--:>DZY Loves Chessboard
+#include <iostream>
+#include <string>
+using namespace std ;
+int main(){
+    int x, y ;
+    cin>>x>>y;
+    string s[y];
+    string res[y];
+    for(int i =0 ; i < y; i++)
+    cin>>s[i];
+    for(int i =0; i < y; i++){
+        for(int j =0 ; j < s[i].length() ; j++){
+                 if((i+1)%2!=0){
+                 if(s[i][j] == '.'){
+                 if(j%2==0)
+                 res[i] += 'B';
+                 else
+                 res[i] += 'W';
+                 }
+                 else if(s[i][j] == '-')
+                 res[i] += '-';
+             }
+             else if((i+1)%2==0){
+                 if(s[i][j] == '.'){
+                 if(j%2!=0)
+                 res[i] += 'B';
+                 else
+                 res[i] += 'W';
+                 }
+                 else if(s[i][j] == '-')
+                 res[i] += '-';
+             }
+             }
+        }
+    for(int i = 0 ; i < y ; i++)
+    cout<<res[i]<<endl;
+    return 0;
+}
