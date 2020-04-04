defd57b73626f93674c072a0d6fe44c4b3cb047e
G2440 <gauravnegi2440@gmail.com>
2020-04-04 22:05:12 +0530

    START

--- 342A.cpp
+++ 342A.cpp
@@ -8,35 +8,45 @@ using namespace std;
      int x; 
      cin>>x;
      int arr[x];
-     int arr1[7] = {0};
+     int arr1[8] = {0};
      for(int i =0 ; i < x;i++){
      cin>>arr[i];
      int t = arr[i];
      arr1[t]++;
      }
-  
      int f =0;
      int y = x/ 3;
+     int arr2[x];
+     int j = 0 ;
      if(x%3==0){
-     while(y--){
+     while(y-- && j<x ){
          if(arr1[1] !=0 && arr1[2] !=0 && arr1[4] !=0){
-             cout<<1<<" "<<2<<" "<<4<<endl;
+             arr2[j] = 1 ; arr2[j+1] = 2;arr2[j+2] = 4;
+             j +=3;
              arr1[1]--;arr1[2]--;arr1[4]--;
              f++;
          }
          else if(arr1[1] !=0 && arr1[2] !=0 && arr1[6] !=0){
-             cout<<1<<" "<<2<<" "<<6<<endl;
+             arr2[j] = 1 ; arr2[j+1] = 2;arr2[j+2] = 6;
+             j +=3;
              arr1[1]--;arr1[2]--;arr1[6]--;
              f++;
          }
          else if(arr1[1] !=0 && arr1[3] !=0 && arr1[6] !=0){
-             cout<<1<<" "<<3<<" "<<6<<endl;
-             arr1[1]--;arr1[3]--;arr1[6]--;
-             f++;
+              arr2[j] = 1 ; arr2[j+1] = 3;arr2[j+2] = 6;
+              j +=3;
+              arr1[1]--;arr1[3]--;arr1[6]--;
+              f++;
          }
      }
      if(f!=x/3)
      cout<<-1<<endl;
+     else{
+         for(int i = 0 ; i < x;i=i+3)
+             cout<<arr2[i]<<" "<<arr2[i+1]<<" "<<arr2[i+2]<<endl;
+         
+
+     }
      }
      else   
      cout<<-1<<endl;
