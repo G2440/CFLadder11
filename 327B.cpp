4cd688fed5371a0d96a3a4dda613f6da441f354d
G2440 <gauravnegi2440@gmail.com>
2020-04-08 22:08:37 +0530

    STR

--- 327B.cpp
+++ 327B.cpp
@@ -3,76 +3,46 @@
 using namespace std;
  
  
-int prime(int n); 
+int isprime(int n); 
 
 int main(){
 
-  int x;
-  cin>>x;
-  int t0 = 5 ;
-  int t1 = 8;
-  int total;
-
-  for(int i = 1 ; i <= x; i++){
-      total = t0 + t1;
-      cout<<total<<" ";
-      t0 = t1;
-      t1 = total;
-      total = 0;
-  }
-
-return 0;
-
+    int x;
+    cin>>x;
+    int  i = 3;
+    int p =0;
+    
+    if(x>1000){
+    int m= 3*x;
+    while(x>p && m <= 10000000){
+        if(m%100!=0){
+        cout<<m<<" ";
+        p++;
+        }
+        m++; 
+    }
+    }
+    else{
+    while(x>p){
+        if(isprime(i)){
+            if(i <= 10000000){
+            cout<<i<<" ";
+            p++;
+            }
+            else 
+            break;
+        }
+        i++;
+    }
+    }
+     return 0;   
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     int x;
-//     cin>>x;
-//     long long int  i = 2;
-//     int p =0;
-//     while(x>p){
-//         if(prime(i)){
-//             if(i <= 10000000){
-//             cout<<i<<" ";
-//             p++;
-//             }
-//             else 
-//             break;
-//         }
-//         i++;
-//     }
-    
-//      return 0;   
-// }
-
-// int prime(int n)
-// {
-//  for (int l = 2; l <= n/2; l+=1) 
-//       if ( n% l == 0) 
-//            return 0;
-//     return 1;          
-// }
+int isprime(int n)
+{
+ for (int l = 2; l <= n/2; l+=1) 
+      if ( n% l == 0) 
+           return 0;
+    return 1;          
+}
