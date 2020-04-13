c7b89d558df82a5390ddb651b4adceee5ac63def
G2440 <gauravnegi2440@gmail.com>
2020-04-13 20:51:15 +0530

    LA

--- 350A.cpp
+++ 350A.cpp
@@ -1,39 +1,41 @@
 //Ladder11--:>TL
 
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     
     int n,m;
+    cin>>n>>m;
     int correct[n],incorrect[m];
     for(int i =0; i < n ; i++)
     cin>>correct[i];
-    for(int i =0; i < n ; i++)
+    for(int i =0; i < m; i++)
     cin>>incorrect[i];
-    int cmin=101,cmax=0;
-
-    for(int i =0 ; i < n ;i++){
-        if(correct[i] < cmin)
-        cmin = correct[i];
-        else if(correct[i] >cmax)
-        cmax = correct[i];
+    
+    int imin = 101;
+    int cmax = 0,cmin=101;
+    for(int i =0 ; i< n;i++)
+     if(correct[i] < cmin)
+    cmin = correct[i];
+    else if(correct[i] >cmax)
+    cmax = correct[i];
+    for(int i =0 ; i< m;i++)
+    if(incorrect[i] < imin)
+    imin = incorrect[i];
+    
+    if(imin <= cmax)
+    cout<<-1<<endl;
+    else
+    {
+        int temp = cmin*2;
+        if(temp <= cmax){
+            if(cmax == imin-1)
+            cout<<cmax<<endl;
+            else  
+            cout<<temp<<endl;
+        }
     }
-    if(cmax>=(2*cmin)){
-       int flag = 0 ;
-       for(int i =0 ; i < m ; i++)
-       if(incorrect[i]<= cmax){
-           flag = 1;
-           break;
-       }
-       if(flag == 1)
-       cout<<-1<<endl;
-       else    
-       cout<<cmax<<endl; 
-    }
-    else{
-        cout<<-1<<endl;
-    }
-
-
+    
     return 0;
 }
