9b470b65252d08a8ce0815107fcf711839ef517b
G2440 <gauravnegi2440@gmail.com>
2020-04-16 23:10:19 +0530

    DONE

--- 160B.cpp
+++ 160B.cpp
@@ -1,26 +1,29 @@
-#include<iostream>
-
+//Ladder11--:>Unlucky Ticket
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     
     int n;
     cin>>n;
-    int x = 2*n;
-    int arr[x];
-    for(int i =0 ; i < x; i++)
-    cin>>arr[i];
+    string arr;
+    cin>>arr;
+    string t = arr.substr(n);
+    string m = arr.substr(0 , n);
+    sort (t.begin(),t.end());
+    sort (m.begin(),m.end());
     int flag1 =0, flag2=0;
-    if(arr[0] > arr[x/2])
+    if(m[0] > t[0])
     flag1 =1;
-    else if(arr[0] < arr[x/2])
+    else if(m[0] < t[0])
     flag2 = 1;
     else
     cout<<"NO"<<endl;
+    
     int flag3=0;
     if(flag1==1){
-      for(int i =1 , j = (x/2 + 1) ; i<x/2 && j < x ; i++,j++)
-          if(arr[i] <= arr[j]){
+      for(int i = 1 ; i<n;i++)
+          if(m[i] <= t[i]){
               flag3 =1;
               break;
           }
@@ -28,11 +31,10 @@ int main(){
           cout<<"NO"<<endl;
           else  
           cout<<"YES"<<endl;
-        
     }
     if(flag2==1){
-        for(int i =1 , j = (x/2 + 1) ; i<x/2 && j < x ; i++,j++)
-          if(arr[i] >= arr[j]){
+        for(int i = 1 ; i < n;i++)
+          if(m[i] >= t[i]){
               flag3 =1;
               break;
           }
@@ -41,6 +43,5 @@ int main(){
           else  
           cout<<"YES"<<endl;
     }
-
     return 0;
 }
