16232621105cca16c5ce3170a0dc8a97ff4f7e8e
G2440 <gauravnegi2440@gmail.com>
2020-04-27 17:03:38 +0530

    DONE

--- 287A.cpp
+++ 287A.cpp
@@ -7,29 +7,42 @@ int main(){
     char arr[4][4];
     int hash = 0, dot =0;
     for(int i =0 ; i < 4; i++)
-        for(int j =0 ; j < 4 ;j++){
+        for(int j =0 ; j < 4 ;j++)
             cin>>arr[i][j];
-            if(arr[i][j] == '#')
+
+    int flag =0;
+    for(int i =0 ; i < 3 ;i++){
+        
+        for(int j =0; j<3 ;j++){
+            hash =0;
+            dot =0;
+            if(arr[i][j]=='#')
             hash++;
             else  
             dot++;
-        }
-    int flag =0;
-    for(int i =0 ; i < 3 ;i++){
-        for(int j =0; j<3;j++){
-            if(arr[i][j]==arr[i][j+1] && arr[i][j+1] == arr[i+1][j+1] && arr[i+1][j+1] == arr[i+1][j]){
+            if(arr[i][j+1]=='#')
+            hash++;
+            else 
+            dot++;
+            if(arr[i+1][j+1] == '#')
+            hash++;
+            else 
+            dot++;
+            if(arr[i+1][j]=='#')
+            hash++;
+            else
+            dot++;
+            if(hash==4 || hash==0 || hash==1 || hash==3){
             flag =1;
             break;
             }
+            }
         }
-    }        
+    
     if(flag == 1)
     cout<<"YES"<<endl;
-    else{
-        if(dot==8 && hash==8)
-        cout<<"NO"<<endl;
-        else  
-        cout<<"YES"<<endl;  
-    }
+    else
+    cout<<"NO"<<endl;      
+    
     return 0;
 }
