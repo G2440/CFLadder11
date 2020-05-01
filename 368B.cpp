2ba1a0bca82f84d321737d8913b70c6a994d3ac0
G2440 <gauravnegi2440@gmail.com>
2020-05-01 23:20:26 +0530

    QU

--- 368B.cpp
+++ 368B.cpp
@@ -7,21 +7,27 @@ int main(){
 
     int n,m;
     cin>>n>>m;
+    int count=0;
     int arr[n];
     for(int i =0 ; i < n;i++){
     cin>>arr[i];
-    dp[arr[i]]++;
     }
-    while(m--){
-        int x;
-        cin>>x;
-        int count =0;
-        int data = arr[x-1];
-        for(int i = 1 ;i<100001;i++)
-        if(dp[i] !=0)
-        count++;
-        dp[data]--;
-        cout<<count<<endl;
+    int arr1[n];
+    int main = 0;
+    for(int i=n-1;i>=0;i--){
+        if(dp[arr[i]]==0){
+            arr1[i] = 1 + main;
+            main++;
+            dp[arr[i]]++;
+        }
+        else{
+            arr1[i] = arr1[i+1];
+        }
     }
+    for(int i = 0; i<m;i++){
+        cin>>count;
+        cout<<arr1[count-1]<<endl;
+    }
+
     return 0;
 }
