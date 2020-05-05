397c814bc3dcaa38d4df34b8e4435a0cb03caa66
G2440 <gauravnegi2440@gmail.com>
2020-05-05 13:04:58 +0530

    dbt

--- 289B.cpp
+++ 289B.cpp
@@ -15,9 +15,15 @@ int main(){
     sort(arr,arr+prod);
     int d=0,count=0;
     d = arr[(prod-1)/2];
+    int check = arr[0]%z;
+    int flag1 =0;
+    if(z==1){
+        flag1 = 1;
+    }
+    if(flag1 ==0){
     int flag =0;
-    for(int i =0 ;i<prod-1;i++){
-        if(arr[i+1] - arr[i] != z){
+    for(int i =1 ;i<prod;i++){
+        if(arr[i]%z!=check){
             flag =1 ;
             break;
         }
@@ -26,6 +32,14 @@ int main(){
     cout<<-1<<endl;
     else{
         for(int i =0 ; i < prod ;i++){
+            int temp = abs(arr[i] - d);
+            count+= temp/z;
+        }
+        cout<<count<<endl;
+    }
+    }
+    else{  
+         for(int i =0 ; i < prod ;i++){
             int temp = abs(arr[i] - d);
             count+= temp/z;
         }
