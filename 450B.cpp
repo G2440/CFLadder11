3b69a959058f8b834ad2ad70d89b15476e4acf49
G2440 <gauravnegi2440@gmail.com>
2020-05-07 19:47:46 +0530

    DONE

--- 450B.cpp
+++ 450B.cpp
@@ -4,36 +4,41 @@
 using namespace std;
 
 lli mod(lli a, lli b);
-lli perm = 1000000007;
+
+
+
 int main(){
-    
+    lli perm = 1000000007;
     lli f1, f2;
     cin>>f1>>f2;
     lli n;
     cin>>n;
-    if(n<=2){
-        if(n==1){
-        lli t1 = mod(f1,perm);
-        cout<<t1<<endl;
-        
-        }else{
-        lli t2 = mod(f2,perm);
-        cout<<t2<<endl;
-    }
-    }
-    else{
-        
-        lli temp0 = f1;
-        lli temp1 = f2;
-        lli sum =0;
-        for(lli i = 3;i<=n;i++){
-            sum += temp1 - temp0;
-            temp0 = temp1;
-            temp1 = sum;
-        }
-        lli t3 = mod(sum,perm);
-        cout<<t3<<endl;
-    }
+    lli x = 6;
+    lli f3 = f2-f1;
+    
+    lli f4 = f3 - f2;
+    
+    lli f5 = f4 - f3;
+    
+    lli f6 = f5 - f4;
+    
+    n = n-1; 
+ 
+    if((n%x) ==0)
+    cout<<mod(f1,perm)<<endl;
+    else if((n%x) == 1)
+    cout<<mod(f2,perm)<<endl;
+    else if((n%x) == 2)
+    cout<<mod(f3,perm)<<endl;
+    else if((n%x) == 3)
+    cout<<mod(f4,perm)<<endl;
+    else if((n%x) == 4)
+    cout<<mod(f5,perm)<<endl;
+    else if((n%x) == 5)
+    cout<<mod(f6,perm)<<endl; 
+    
+
+
 
     return 0;
 }
